f8518b3dffca50e41cfb9619732255e1dd7590f9
Rodrigo monroy <ruimonroy@csu.fullerton.edu>
2016-12-26 16:06:52 -0600

    new website

---
+++ js/menuController.js
@@ -0,0 +1,30 @@
+/*****************************************************
+    File to take care of all the menu events
+******************************************************/
+var menu = function()
+{
+  	'use strict';
+    $('.item').on('click',function(e)
+        {
+            e.preventDefault();
+            var nextRef = $(this).attr('href');
+            var display = $('#display div').toArray();
+            $(display).transition({ animation :'slide right',
+                                    duration  : '700ms'})
+                      .transition('hide');
+
+            $(nextRef).transition({ animation :'slide left',
+                                    duration  : '700ms',
+                                    onStart: function()
+                                    {
+                                        var nextRefFields=$(nextRef +' div').toArray();
+                                        console.log(nextRefFields);
+                                        $(nextRefFields).transition({ animation :'slide left',
+                                                                     duration  : '500ms'
+                                                                    });
+                                    }
+                                   });
+
+        });
+};
+$(document).ready(menu);
