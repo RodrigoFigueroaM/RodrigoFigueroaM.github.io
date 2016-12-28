49c44737ed83467b28d6db7b827db693d3d60c7f
Rodrigo monroy <ruimonroy@csu.fullerton.edu>
2016-12-28 00:33:49 -0600

    made more responsive transition

--- js/menuController.js
+++ js/menuController.js
@@ -4,27 +4,39 @@
 var menu = function()
 {
   	'use strict';
-    $('.item').on('click',function(e)
-        {
-            $('#menu .item').prop( "disabled", true );
-            e.preventDefault();
-            var nextRef = $(this).attr('href');
-            var display = $('#display div').toArray();
-            $(display).transition({ animation :'slide right',
-                                    duration  : '700ms'})
-                      .transition('hide');
+    $('.item').on('click',clickOnMenuItem);
 
-            $(nextRef).transition({ animation :'slide left',
-                                    duration  : '700ms',
-                                    onStart: function()
+};
+
+var clickOnMenuItem = function(e)
+    {
+        e.preventDefault();
+        $('.item').unbind('click', clickOnMenuItem);
+        var nextRef = $(this).attr('href');
+        var display = $('#display div').toArray();
+
+        $(display).transition({ animation :'slide right',
+                                duration  : '500ms'
+                            })
+                  .transition('hide');
+
+        $(nextRef).transition({ animation :'slide left',
+                                duration  : '500ms',
+                                onStart: function()
+                                {
+                                    if($(nextRef +' div'))
                                     {
                                         var nextRefFields=$(nextRef +' div').toArray();
-                                         $(nextRefFields).transition({ animation :'slide left',
+                                        $(nextRefFields).transition({ animation :'slide left',
                                                                      duration  : '500ms'
                                                                     });
                                     }
-                                   });
-
-        });
+                                },
+                                onComplete: function()
+                                {
+                                    $('.item').bind('click', clickOnMenuItem);
+                                }
+                            });
 };
+
 $(document).ready(menu);
